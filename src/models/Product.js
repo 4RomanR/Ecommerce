05fac5327c1070ab22f949ceec38349f5e89ddfb
4Roman4 <4rprogramming@gmail.com>
2024-02-21 23:52:23 -0400

    Testing Categories

---
+++ src/models/Product.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utils/connection');
+
+const Product = sequelize.define('product', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique:true
+    },
+    description: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+         unique:true
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false
+    },
+    //categoryId
+});
+
+//User.prototype.toJSON = function () {
+//	const values = { ...this.get() };
+//	delete values.password ;
+//	return values;
+//};
+module.exports = Product;
--- src/routes/category.router.js
+++ src/routes/category.router.js
@@ -1,14 +1,15 @@
 const { verify } = require('jsonwebtoken');
 const { getAll, create, remove } = require('../controllers/category.controllers');
 const express = require('express');
+const { verifyJwt } = require('../utils/verifyJWT');
 
 const routerCategory = express.Router();
 
 routerCategory.route('/')
-    .get(verify, getAll)
-    .post(verify, create);
+    .get(verifyJwt, getAll)
+    .post(verifyJwt, create);
 
 routerCategory.route('/:id')
-    .delete(verify, remove)
+    .delete(verifyJwt, remove)
 
 module.exports = routerCategory;
